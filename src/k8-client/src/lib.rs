4d84faf4e84cc8dc7ea68965f6240489c6b6572e
Sehyo Chang <sehyo@fluvio.io>
2020-02-12 11:36:48 -0800

    reexport K8Config add shared definition

--- src/k8-client/src/lib.rs
+++ src/k8-client/src/lib.rs
@@ -8,11 +8,30 @@ mod stream;
 #[cfg(feature = "k8")]
 pub mod fixture;
 
-
 pub use self::client::K8Client;
 pub use self::config::K8HttpClientBuilder;
 pub use self::error::ClientError;
+pub use k8_config::K8Config;
 
 pub mod metadata {
     pub use k8_metadata_client::*;
 }
+
+pub use shared::SharedK8Client;
+pub use shared::new_shared;
+
+mod shared {
+
+    use std::sync::Arc;
+    use super::K8Config;
+    use super::ClientError;
+    use super::K8Client;
+
+    pub type  SharedK8Client = Arc<K8Client>;
+
+    pub fn new_shared(config: K8Config) -> Result<SharedK8Client,ClientError> {
+        let client= K8Client::new(config)?;
+        Ok(Arc::new(client))
+    }
+}
+
