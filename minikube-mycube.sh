9469838dc725caf687e2adaca3366f3ce93fe7f2
Sehyo Chang <sehyo@fluvio.io>
2022-11-19 14:04:31 -0800

    remove obsolet file

--- minikube-mycube.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export IP=$(minikube ip)
-echo "$IP minikubeCA" | sudo tee -a  /etc/hosts
-cd ~
-kubectl config set-cluster mycube --server=https://minikubeCA:8443 --certificate-authority=.minikube/ca.crt
-kubectl config set-context mycube --user=minikube --cluster=mycube
-kubectl config use-context mycube
